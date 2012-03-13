e8d29fb17c7f6d643f877927c9c05c54e02b81b5
blake.raymond.dev <blake.raymond@aquaticinformatics.com>
2012-03-12 19:00:26 -0700

    More messing around with Pantheios trivial logging

--- Pantheios_and_Boost/Pantheios/Pantheios.cpp
+++ Pantheios_and_Boost/Pantheios/Pantheios.cpp
@@ -3,6 +3,7 @@
 #pragma once
 #include "stdafx.h"
 
+//Process identity appended to all log messages
 extern "C" const char PANTHEIOS_FE_PROCESS_IDENTITY[] = "LogToConsole";
 
 struct TsPoint
@@ -46,7 +47,7 @@ private:
 		if('\0' != m_value[0] ||
 			  0 != m_length) throw std::exception();
 
-		m_length = static_cast<size_t>(::sprintf(&m_value[0], "{Time: %f, Value: %f", m_tspoint._time, m_tspoint._value));
+		m_length = static_cast<size_t>(::sprintf(&m_value[0], "Time: %f, Value: %f", m_tspoint._time, m_tspoint._value));
 	}
 	
 	TsPoint const& m_tspoint;
@@ -92,6 +93,17 @@ int _tmain(int argc, _TCHAR* argv[])
 		//TsPoint insertion via custom inserter class
 		TsPoint point(40000, 10);
 		pantheios::log_DEBUG("TsPoint inserter: ", tsPoint_inserter(point));
+
+		//Up to 32 (or so) items in a log request
+		pantheios::log_DEBUG("I ", "can ", "split ", "my ", "log ",
+							 "message ", "up ", "unnecessarily ");
+		pantheios::log_DEBUG("Everything ends up as a narrow string \
+							  so I can add any kind of data in any order",
+							  pantheios::integer(13), "/", 
+							  pantheios::w2m(L"AI Rocks"), "/", 
+							  tsPoint_inserter(TsPoint(13.0, 13.0)));
+		
+		pantheios::log_WARNING("The party is about to start");
 
         return EXIT_SUCCESS;
     }
